db109c89f583f12ba954d2b11555c3fdb3347793
Saynick <oscarmartinezsalazar@gmail.com>
2022-09-26 15:00:26 -0500

    COMMIT

---
+++ includes/funciones.php
@@ -0,0 +1,18 @@
+<?php
+
+function ObtenerServicios(){
+    try{
+
+        //Importar una conexión
+
+        require 'database.php';
+
+        //Escribir el código SQL
+
+        //Obtener los resultados
+
+    }catch (\Throwable $th){
+
+        var_dump($th);
+    }
+}
