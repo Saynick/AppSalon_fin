4370a9c9707937d7468d0c12af5765d2b6832850
Saynick <oscarmartinezsalazar@gmail.com>
2022-09-26 17:47:52 -0500

    CAMBIOS

--- includes/funciones.php
+++ includes/funciones.php
@@ -1,18 +1,30 @@
 <?php
 
-function ObtenerServicios(){
+function obtenerServicios(){
     try{
 
         //Importar una conexión de BD
 
         require 'database.php';
-
+        
         //Escribir el código SQL
 
+        $sql = "SELECT * FROM servicios;";
+
+        $consulta = mysqli_query($db, $sql);
+
+
         //Obtener los resultados
+        
+        echo "<pre>";
+        var_dump(mysqli_fetch_assoc($consulta));
+
+        echo "</pre>";
 
     }catch (\Throwable $th){
 
         var_dump($th);
     }
 }
+
+obtenerServicios();
